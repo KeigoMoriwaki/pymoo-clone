3406b344e82b205947de7753527ea094ec793650
KeigoMoriwaki <167503704+KeigoMoriwaki@users.noreply.github.com>
2024-07-14 23:12:54 +0900

    Add files via upload

--- project_scheduling/problem.py
+++ project_scheduling/problem.py
@@ -36,27 +36,36 @@ class ResourceConstrainedSchedulingProblem(Problem):
         for ind in x:
             schedule = ind.reshape((len(self.J), self.T))
             leftover = copy.deepcopy(self.p)
+            resource_constraints = 0
             t = 0
-            for t, s in enumerate(schedule.T):
-                for i, j in enumerate(s):
-                    if j != 0:
-                        leftover[i + 1] = max(0, leftover[i + 1] - 1)
+            for t in range(self.T):
+                n_worker = np.zeros(len(self.R))
+                for j in range(len(self.J)):
+                    robot = schedule[j, t]
+                    if robot != 0:
+                        if n_worker[robot-1] > 0:
+                            resource_constraints += 1  # 同じロボットに複数のタスクが割り当てられた場合のペナルティ
+                        n_worker[robot-1] += 1
+                        leftover[self.J[j]] -= 1
+                        if leftover[self.J[j]] <= 0:
+                            leftover[self.J[j]] = 0
+
                 if sum(leftover.values()) == 0:
                     break
-            finish_times.append(t + 1)  # タスクが終了した時刻を記録
 
-            resource_constraints = []
-            for t, s in enumerate(schedule.T):
+            finish_times.append(t + 1)
+
+            for t in range(self.T):
                 n_worker = np.zeros(len(self.R))
-                for i, j in enumerate(s):
-                    if j != 0:
-                        n_worker[j-1] += 1
+                for j in range(len(self.J)):
+                    robot = schedule[j, t]
+                    if robot != 0:
+                        n_worker[robot-1] += 1
                 for r in range(len(self.R)):
-                    if self.RUB[(r+1, t+1)] - n_worker[r] < 0:
-                        resource_constraints.append(self.RUB[(r+1, t+1)] - n_worker[r])
-            if len(resource_constraints) == 0:
-                resource_constraints.append(0)
-            constraints.append(sum(resource_constraints))
+                    if self.RUB[(r+1, t+1)] < n_worker[r]:
+                        resource_constraints += 1
 
-        out["F"] = np.array(finish_times).reshape(-1, 1)  # 正しい形状に変換
-        out["G"] = np.array(constraints).reshape(-1, 1)  # 正しい形状に変換
+            constraints.append(resource_constraints)
+
+        out["F"] = np.array(finish_times).reshape(-1, 1)
+        out["G"] = np.array(constraints).reshape(-1, 1)
